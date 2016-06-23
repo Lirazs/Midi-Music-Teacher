1c962b4f5258110d5fd8f64f781de34c7c693a80
Omer <blah@bloop.bla>
2016-06-23 13:54:39 +0300

    notes annotation

--- score.js
+++ score.js
@@ -27,32 +27,41 @@ Score.Stave.prototype.clear = function() {
 };
 
 
-Score.Stave.prototype.drawBeat = function(notes, duration) {
+Score.Stave.prototype.drawBeat = function(notes, duration, annotation) {
    var self = this;
 
    var voice_notes = new Vex.Flow.Voice({ num_beats: 1, beat_value: duration, resolution: Vex.Flow.RESOLUTION });
    voice_notes.addTickables([self._makeBeat(notes, duration)]);
-   self._draw(voice_notes);
+
+   var text_notes = new Vex.Flow.Voice({ num_beats: 1, beat_value: duration, resolution: Vex.Flow.RESOLUTION });
+   text_notes.addTickables([self._beatAnnotation(duration, annotation)]);
+
+   self._draw(voice_notes, text_notes);
 };
 
 
 Score.Stave.prototype.drawSequence = function(beats) {
    var self = this;
 
-
    var seq = self._makeSequence(beats);
+
    var voice_notes = new Vex.Flow.Voice({ num_beats: seq.num_beats, beat_value: seq.beat_value, resolution: Vex.Flow.RESOLUTION });
    voice_notes.addTickables(seq.notes);
-   self._draw(voice_notes);
+
+   var text_notes = new Vex.Flow.Voice({ num_beats: seq.num_beats, beat_value: seq.beat_value, resolution: Vex.Flow.RESOLUTION });
+   text_notes.addTickables(seq.annotation);
+
+   self._draw(voice_notes, text_notes);
 };
 
 
-Score.Stave.prototype._draw = function(voice) {
+Score.Stave.prototype._draw = function(voice, annotation) {
    var self = this;
    self._ctx.clearRect(0, 0, self._canvas.width, self._canvas.height);
-   var formatter = new Vex.Flow.Formatter().joinVoices([voice]).format([voice], self._width);
+   var formatter = new Vex.Flow.Formatter().joinVoices([voice, annotation]).format([voice, annotation], self._width);
    self._stave.draw();
    voice.draw(self._ctx, self._stave);
+   annotation.draw(self._ctx, self._stave);
 };
 
 
@@ -65,30 +74,41 @@ Score.Stave.prototype._makeBeat = function(notes, duration) {
       keys.push(note_info.name+"/"+(note_info.octave || '4'));
    });
    
-   var note = new Vex.Flow.StaveNote({ keys: keys, duration: duration.toString(), clef: self._clef_name, auto_stem: true });
+   var stave_note = new Vex.Flow.StaveNote({ keys: keys, duration: duration.toString(), clef: self._clef_name, auto_stem: true });
 
    for (var i = 0; i < notes.length; ++i) {
       if (notes[i].accidental) {
-         note.addAccidental(i, new Vex.Flow.Accidental(notes[i].accidental));
+         stave_note.addAccidental(i, new Vex.Flow.Accidental(notes[i].accidental));
       }
    }
-   return note;
+   return stave_note;
 };
+
+Score.Stave.prototype._beatAnnotation = function(duration, annotation) {
+   var self = this;
+   var annotation = annotation || {text: '', position: 0};
+   var text_note = new Vex.Flow.TextNote({ text: annotation.text,
+                                           font: {family: 'Arial', size: 10, weight: ''},
+                                           duration: duration.toString() }).setLine(annotation.position).setJustification(Vex.Flow.TextNote.Justification.LEFT).setContext(self._ctx);
+   return text_note;
+};
+
 
 
 Score.Stave.prototype._makeSequence = function(beats) {
    var self = this;
 
    var stave_notes = [];
+   var text_notes = [];
    var total_duration = 0;
    beats.forEach(function (beat_info) {
       stave_notes.push(self._makeBeat(beat_info.notes, beat_info.duration));
+      text_notes.push(self._beatAnnotation(beat_info.duration, beat_info.annotation));
       total_duration += 32/beat_info.duration;
    });
 
-   return {notes: stave_notes, num_beats: total_duration, beat_value: 32};
+   return {notes: stave_notes, num_beats: total_duration, beat_value: 32, annotation: text_notes};
 };
-
 
 
 
