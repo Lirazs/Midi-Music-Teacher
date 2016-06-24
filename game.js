ecb6d78855b797ae1633d778b1f7545b456eff10
Omer <blah@bloop.bla>
2016-06-25 00:09:52 +0300

    stage 01

--- game.js
+++ game.js
@@ -1,5 +1,6 @@
 (function(Game) {
 
+// Stage Selector
 // Nice Feedback
 
 
@@ -8,7 +9,7 @@ Game.Chapter1 = function(quizzer, octave, timeout, allowed_keys) {
    self._quizzer = quizzer;
    self._octave = octave;
    self._timeout = timeout;
-   self._allowed_keys = allowed_keys;
+   self._quizzer.restrictKeys(allowed_keys);
 
    octave.annotation(false);
 };
@@ -36,5 +37,36 @@ Game.Chapter1.prototype.onRelease = function(octave, note_index) {
    var self = this;
    self._octave.releaseKey(note_index);
 };
+
+
+
+Game.initStage1 = function init() {
+   document.getElementById('asked-note-symbol').querySelector('canvas').style.display = 'none';
+   document.getElementById('asked-note-symbol').querySelector('span').style.display = 'inline-block';
+
+   var stave = new Score.Stave("stave", "treble");
+   var octave = new Keyboard.Octave(document.getElementById('octave'));
+   var quizzer = new Quizzer.NotesGame('user-points', 'timer', 'asked-note-name', stave);
+
+   var game = new Game.Chapter1(quizzer, octave, 5000, [0,2,4,5,7,9,11]);
+
+   var midi = new Midi.Controller(
+      function(ok_flag, devices, msg) {
+         if (!ok_flag) {
+            alert('Midi is unsupported by the browser: ' + msg);
+            return;
+         }
+         Common.initDeviceList(devices, document.getElementById('devices-list'));
+         game.newRound();
+      },
+      function(octave, note_index) {
+         game.onPress(octave, note_index);
+      },
+      function(octave, note_index) {
+         game.onRelease(octave, note_index);
+      }
+   );
+};
+
 
 }(window.Game = window.Game|| {}));
